ce6df668ab8474c1c15b36f5a8d3c6e02e9ba8b5
q90016200 <q90016200@gmail.com>
2021-04-12 18:20:26 +0800

    修改表 precount-fix 中欄位 time 時區更為 UTC + 0

--- src/AsyncSettled.php
+++ src/AsyncSettled.php
@@ -413,7 +413,7 @@ class AsyncSettled
                     "bet_amount" => $asyncSettledLog["bet_amount"],
                     "win_amount" => $asyncSettledLog["win_amount"],
                     "game_code" => $asyncSettledLog["game_code"],
-                    "time" => $lst->format("Y-m-d H"),
+                    "time" => $lst->copy()->subHour(8)->format("Y-m-d H"),
                     "created_at" => new UTCDateTime()
                 ];
                 $this->mongodb->setPool($this->mongoDefaultPool)->insert($this->prcountFixCol, $pfRecord);
